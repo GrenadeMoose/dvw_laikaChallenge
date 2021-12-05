23fc2f3ca2bfc67568d400dca9f461bdae798bff
GrenadeMoose <danvanwagnen@gmail.com>
2021-12-05 11:09:51 -0500

    Adding HTML output
    Changed the function inputs to include all entities and fields as a single dict
    
    Added html export
    
    Still having an issue getting a definite return value from field query. Is this related to shot versioning?

--- codeChallenge.py
+++ codeChallenge.py
@@ -6,18 +6,25 @@ html_out_dir = os.path.join(
     "html"
 )
 
+server_path = "https://laika-demo.shotgunstudio.com"
+script_name = "code_challenge"
+api_key = "$zvMznkhddo0tgwgwbftzaqob"
 
-def shotgun_challenge(id, sequence_fields, shot_fields):
+
+def shotgun_challenge(id, queries):
     """
     Initializes the connection to the shotgun server.
-    Pulls requested information.
-    Exports info to an HTML table
+    Queries requested information.
+    Calls save_to_html to handle exporting query to html table.
+
+    :param id: Sequence id to be queried
+    :param queries: a dict of entity type strings and field lists to be queried
     """
 
     shotgun_api3.shotgun.NO_SSL_VALIDATION = True
-    sg = shotgun_api3.Shotgun("https://laika-demo.shotgunstudio.com",
-                              script_name="code_challenge",
-                              api_key="$zvMznkhddo0tgwgwbftzaqob")
+    sg = shotgun_api3.Shotgun(server_path,
+                              script_name=script_name,
+                              api_key=api_key)
     # Be sure to close connection when you are done.
     sg.connect()
 
@@ -25,17 +32,52 @@ def shotgun_challenge(id, sequence_fields, shot_fields):
         ['project', 'is', {'type': 'Project', 'id': id}]
     ]
 
-    shots = sg.find("Shot", filters, shot_fields)
-    sequences = sg.find("Sequence", filters, sequence_fields)
-    
-    for s in shots:
-        print(sg.schema_field_read("Shot", field_name="sg_latest_version", project_entity=s))
+    for entity, fields in queries.items():
+        res = sg.find(entity, [['project.Project.id', 'is', id]], fields)
+        save_to_html(res, entity, fields)
 
     sg.close()
 
 
+def save_to_html(query_results, entity, fields):
+    """
+    Saving the query results to an HTML file.
+
+    :param query_results: The results of the shotgun_challenge query as a list of dicts
+    :param entity: String identifier for the entity
+    :param fields: String list of fields queried in shotgun_challenge
+
+    :output: html table of query results saved to html_out_dir
+    """
+    table = ["<table>", "\t<tr>"]
+
+    # Append headers
+    table.append("\t\t<th>Type</th>")
+    table.append("\t\t<th>ID</th>")
+    for field in fields:
+        table.append("\t\t<th>{0}</th>".format(field))
+    table.append("\t</tr>")
+
+    # Append values
+    for r in query_results:
+        table.append("\t\t<th>{0}</th>".format(r['type']))
+        table.append("\t\t<th>{0}</th>".format(r['id']))
+        for field in fields:
+            table.append("\t\t<th>{0}</th>".format(r[field]))
+        table.append("\t</tr>")
+
+    table.append("</table>")
+
+    if not os.path.exists(html_out_dir):
+        os.mkdir(html_out_dir)
+
+    with open(os.path.join(html_out_dir, "{}.html".format(entity)), 'w') as outfile:
+        outfile.write('\n'.join(table))
+
+
 if __name__ == "__main__":
-    shotgun_challenge(id=85,
-                      sequence_fields=["sg_cut_duration",
-                                       "sg_ip_versions"],
-                      shot_fields=["sg_latest_version"])
+    shotgun_challenge(85,
+                      {"Sequence": ["sg_cut_duration",
+                                    "sg_ip_versions"],
+                       "Shot": ["sg_latest_version"]
+                       })
